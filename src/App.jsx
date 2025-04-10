ac809a4c996489196605c26f928bcddee2e4235a
BoredDarkness <gwinnblaidd@gmail.com>
2025-04-10 08:36:01 +0200

    is it works?

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,66 @@
 import React, { useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchContacts } from "./redux/contactsOps";
-import { selectLoading, selectError } from "./redux/contactsSlice";
+import { refreshUser } from "./redux/auth/operations";
+import { selectIsRefreshing } from "./redux/auth/selectors";
+import { Toaster } from "react-hot-toast";
 
-import ContactsForm from "./components/ContactForm/ContactForm";
-import SearchBox from "./components/SearchBox/SearchBox";
-import ContactList from "./components/ContactList/ContactList";
-import styles from "./App.module.css";
+import Layout from "./components/Layout/Layout";
+import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import RestrictedRoute from "./components/RestrictedRoute/RestrictedRoute";
+
+import HomePage from "./pages/HomePage/HomePage";
+import RegistrationPage from "./pages/RegistrationPage/RegistrationPage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import ContactsPage from "./pages/ContactsPage/ContactsPage";
 
 function App() {
   const dispatch = useDispatch();
-  const loading = useSelector(selectLoading);
-  const error = useSelector(selectError);
+  const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
-    dispatch(fetchContacts());
+    dispatch(refreshUser());
   }, [dispatch]);
 
+  if (isRefreshing) {
+    return <p>Refreshing user...</p>;
+  }
+
   return (
-    <div className={styles.app}>
-      <h1 className={styles.title}>Budnect</h1>
-      <p className={styles.subtitle}>connect your buddies</p>
-      <ContactsForm />
-      <SearchBox />
-      {loading && <p>Loading contacts...</p>}
-      {error && <p style={{ color: "red" }}>{error}</p>}
-      <ContactList />
-    </div>
+    <>
+      {}
+      <Toaster position="top-right" reverseOrder={false} />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route
+            path="/register"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<RegistrationPage />}
+              />
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <RestrictedRoute
+                redirectTo="/contacts"
+                component={<LoginPage />}
+              />
+            }
+          />
+          <Route
+            path="/contacts"
+            element={
+              <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+            }
+          />
+          <Route path="*" element={<HomePage />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
